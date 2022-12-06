5a179b2cce5ab5aa4bab2463dc855f8f0cbd83d9
Elena <zagre8elena@yandex.ru>
2022-12-06 19:31:59 +0300

    add FillArray and PrintArray

--- Taks/Program.cs
+++ Taks/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача: Написать программу, которая из имеющегося массива строк
+// формирует массив из строк, длина которыхменьше либо равна 3 символа.
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
+string[] FillArray(int n)
+{
+    string[] textarray = new string[n];
+    for (int i = 0; i < n; i++)
+    {
+        textarray[i] = Console.ReadLine();
+    }
+    return textarray;
+}
+void PrintArray(string[] text)
+{
+    Console.Write("[ ");
+    foreach (string item in text)
+    {
+        Console.Write($"\"{item}\" ");
+    }
+    Console.Write("]");
+}
+Console.Write("Введите количество элементов массива  ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите элементы массива с клавиатуры, разделяя их клавишей 'enter'");
+string[] array = FillArray(n);
+PrintArray(array);
+Console.Write(" -> ");
